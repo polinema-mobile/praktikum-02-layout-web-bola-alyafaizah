af3ffdae53c36c4d6bce722ed4977829ac700081
alyafaizah <alya.faizah01@gmail.com>
2021-02-16 13:08:10 +0700

    menambahkan gambar

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,20 @@ class MyApp extends StatelessWidget {
           title: Text("MyApp"),
           backgroundColor: Colors.red
         ),
-        body: Container(
-          alignment:Alignment.topCenter,
-          child:Text(
-            'BERITA TERBARU       PERTANDINGAN HARI INI',
+        body: Column(
+          children:[
+            Container(
+              alignment: Alignment.topCenter,
+              child:Text(
+             'BERITA TERBARU       PERTANDINGAN HARI INI',
             style: TextStyle(fontSize:14,height: 3.0),
+          ),
+          ),
+          Container(
+            color:Colors.purpleAccent,
+            child: Image(image: NetworkImage('https://cdns.klimg.com/bola.net/library/upload/21/2021/02/luis-suarez_ec86d1f.jpg'),),
           )
+          ]
         )
       ),
     );
