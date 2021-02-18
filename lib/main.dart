e40430fe4b8ad8ba9cbdc3b719475c900466760f
alyafaizah <alya.faizah01@gmail.com>
2021-02-18 13:37:40 +0700

    menambah gambar dan tulisan dengan border

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,49 @@ class MyApp extends StatelessWidget {
           children:[
             Container(
               alignment: Alignment.topCenter,
+              padding: EdgeInsets.only(bottom:15),
               child:Text(
              'BERITA TERBARU       PERTANDINGAN HARI INI',
             style: TextStyle(fontSize:14,height: 3.0),
           ),
           ),
           Container(
-            color:Colors.purpleAccent,
+            decoration: BoxDecoration(
+              border: Border(
+                left: BorderSide(
+                  color:Colors.purpleAccent,
+                ),
+                top: BorderSide(
+                  color:Colors.purpleAccent,
+                ),
+                right: BorderSide(
+                  color:Colors.purpleAccent,
+                ),
+              ),
+            ),
             child: Image(image: NetworkImage('https://cdns.klimg.com/bola.net/library/upload/21/2021/02/luis-suarez_ec86d1f.jpg'),),
+          ),
+          Center(
+            child:Container(
+              decoration: BoxDecoration(
+              border: Border(
+                left: BorderSide(
+                  color:Colors.purpleAccent,
+                ),
+                bottom: BorderSide(
+                  color:Colors.purpleAccent,
+                ),
+                right: BorderSide(
+                  color:Colors.purpleAccent,
+                ),
+              ),
+            ),
+              margin : const EdgeInsets.all(0.0),
+              padding : const EdgeInsets.all(10.0),
+              alignment: Alignment.topCenter,
+              child: Text('Costa Mendekat Ke Palmeiras' ,
+              style: TextStyle(fontSize: 18, fontWeight:FontWeight.bold  ),
+              ),
           )
           ]
         )
